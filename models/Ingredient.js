588f7f33fd33395667b5e2d1c9fe71d48c79b0f4
Travis Gerrard <travisgerrard@gmail.com>
2020-11-08 15:33:26 -0800

    updated ingredent catergory options

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -1,10 +1,38 @@
-import { Text, Relationship } from '@keystonejs/fields';
+import { Text, Relationship, Select } from '@keystonejs/fields';
 import { userIsAdminOrOwner, userCanUpdateItem } from '../utils/access';
+import { CloudinaryImage } from '@keystonejs/fields-cloudinary-image';
+import { CloudinaryAdapter } from '@keystonejs/file-adapters';
+
+const cloudinaryAdapter = new CloudinaryAdapter({
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  apiKey: process.env.CLOUDINARY_KEY,
+  apiSecret: process.env.CLOUDINARY_SECRET,
+  folder: 'veggily',
+});
+
+const options = [
+  { value: 0, label: 'none' },
+  { value: 1, label: 'produce' },
+  { value: 2, label: 'bakery' },
+  { value: 3, label: 'frozen' },
+  { value: 4, label: 'baking & spices' },
+  { value: 5, label: 'nuts, seeds & dried fruit' },
+  { value: 6, label: 'rice, grains & beans' },
+  { value: 7, label: 'canned & jarred goods' },
+  { value: 8, label: 'oils, sauces & condiments' },
+  { value: 9, label: 'ethnic' },
+  { value: 10, label: 'dairy & cheese' },
+];
 
 export default {
   fields: {
     name: { type: Text, isUnique: true, isRequired: true },
     meal: { type: Relationship, ref: 'Meal', many: true },
+    category: { type: Select, options, dataType: 'integer', defaultValue: 0 },
+    ingredientImage: {
+      type: CloudinaryImage,
+      adapter: cloudinaryAdapter,
+    },
   },
   // access: {
   //   create: true,
