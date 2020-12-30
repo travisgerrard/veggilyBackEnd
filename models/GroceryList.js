4c0a8c79a25547a5a7aa4fdf35dd29642f9b6278
Travis Gerrard <travisgerrard@gmail.com>
2020-12-30 12:09:48 -0800

    added meal component to grocery list

--- models/GroceryList.js
+++ models/GroceryList.js
@@ -1,17 +1,18 @@
-import { Relationship, Checkbox, DateTime } from "@keystonejs/fields";
+import { Relationship, Checkbox, DateTime } from '@keystonejs/fields';
 
 export default {
   fields: {
-    ingredient: { type: Relationship, ref: "Ingredient", many: false },
-    amount: { type: Relationship, ref: "Amount", many: false },
-    author: { type: Relationship, ref: "User.groceryList", many: false },
+    ingredient: { type: Relationship, ref: 'Ingredient', many: false },
+    amount: { type: Relationship, ref: 'Amount', many: false },
+    author: { type: Relationship, ref: 'User.groceryList', many: false },
+    meal: { type: Relationship, ref: 'Meal', many: false },
     isCompleted: { type: Checkbox, defaultValue: false },
     dateCompleted: {
       type: DateTime,
-      format: "dd/MM/yyyy HH:mm O",
+      format: 'dd/MM/yyyy HH:mm O',
       yearRangeFrom: 1901,
       yearRangeTo: 2018,
-      yearPickerType: "auto",
+      yearPickerType: 'auto',
     },
   },
 };
--- models/Meal.js
+++ models/Meal.js
@@ -1,45 +1,42 @@
-import {
-  Text,
-  DateTime,
-  Relationship,
-} from "@keystonejs/fields";
+import { Text, DateTime, Relationship } from '@keystonejs/fields';
 import { CloudinaryImage } from '@keystonejs/fields-cloudinary-image';
 import { CloudinaryAdapter } from '@keystonejs/file-adapters';
-import {
-    userIsAdminOrOwner,
-    userCanUpdateItem,
-  } from '../utils/access';
+import { userIsAdminOrOwner, userCanUpdateItem } from '../utils/access';
 
 const cloudinaryAdapter = new CloudinaryAdapter({
   cloudName: process.env.CLOUDINARY_CLOUD_NAME,
   apiKey: process.env.CLOUDINARY_KEY,
   apiSecret: process.env.CLOUDINARY_SECRET,
-  folder: "veggily",
+  folder: 'veggily',
 });
 
 export default {
   fields: {
-    mealImage: { type: CloudinaryImage, adapter: cloudinaryAdapter, isRequired: true },
+    mealImage: {
+      type: CloudinaryImage,
+      adapter: cloudinaryAdapter,
+      isRequired: false,
+    },
     name: { type: Text, isRequired: true },
     description: { type: Text, isRequired: true },
     createdAt: {
       type: DateTime,
-      format: "MM/DD/YYYY h:mm A",
+      format: 'MM/DD/YYYY h:mm A',
       yearRangeFrom: 1901,
       yearRangeTo: 2018,
-      yearPickerType: "auto",
+      yearPickerType: 'auto',
     },
-    author: { type: Relationship, ref: "User.meals", many: false },
+    author: { type: Relationship, ref: 'User.meals', many: false },
     ingredientList: {
       type: Relationship,
-      ref: "MealIngredientList.meal",
+      ref: 'MealIngredientList.meal',
       many: true,
     },
   },
-//  access: {
-//    create: true,
-//    read: true,
-//    update: userCanUpdateItem,
-//    delete: userIsAdminOrOwner,
-//  },
+  //  access: {
+  //    create: true,
+  //    read: true,
+  //    update: userCanUpdateItem,
+  //    delete: userIsAdminOrOwner,
+  //  },
 };
