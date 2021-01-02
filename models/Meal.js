b18dc4ef5d8fcaecd8e2ff8c082e9603bc426145
Travis Gerrard <travisgerrard@gmail.com>
2021-01-01 19:24:55 -0800

    added at tracking

--- models/Meal.js
+++ models/Meal.js
@@ -2,6 +2,7 @@ import { Text, DateTime, Relationship } from '@keystonejs/fields';
 import { CloudinaryImage } from '@keystonejs/fields-cloudinary-image';
 import { CloudinaryAdapter } from '@keystonejs/file-adapters';
 import { userIsAdminOrOwner, userCanUpdateItem } from '../utils/access';
+const { atTracking } = require('@keystonejs/list-plugins');
 
 const cloudinaryAdapter = new CloudinaryAdapter({
   cloudName: process.env.CLOUDINARY_CLOUD_NAME,
@@ -19,13 +20,14 @@ export default {
     },
     name: { type: Text, isRequired: true },
     description: { type: Text, isRequired: true },
-    createdAt: {
-      type: DateTime,
-      format: 'MM/DD/YYYY h:mm A',
-      yearRangeFrom: 1901,
-      yearRangeTo: 2018,
-      yearPickerType: 'auto',
-    },
+    // createdAt: {
+    //   type: DateTime,
+    //   format: 'MM/DD/YYYY h:mm A',
+    //   yearRangeFrom: 1901,
+    //   yearRangeTo: 2018,
+    //   yearPickerType: 'auto',
+    //   defaultValue: ISODate(new Date()),
+    // },
     author: { type: Relationship, ref: 'User.meals', many: false },
     ingredientList: {
       type: Relationship,
@@ -33,6 +35,7 @@ export default {
       many: true,
     },
   },
+  plugins: [atTracking()],
   //  access: {
   //    create: true,
   //    read: true,
